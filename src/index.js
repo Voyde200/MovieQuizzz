ad05fcbcd305d87b6be30b0455bf3bb9331c4c47
Voyde200 <antoine.schirr@hetic.net>
2024-03-06 13:55:00 +0100

    Creation of a ReactJS app
    Creation of the base of our ReactJS app, we will code on this common base

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import TestCompnent from "./Poil";
+
+ReactDOM.render(<TestCompnent />, document.getElementById("root"));
