6246e3ecae5d59660d79a7e2b7c70eff00c7cc50
Voyde200 <97736322+Voyde200@users.noreply.github.com>
2024-03-08 13:53:29 +0100

    Question prompt (#2)
    * Fetch of 10 movies in the API
    
    Fetch of 10 movies in the API, using a keyword, this is mandatory because the API won't let me fecth completly randomly. The fecth works only in the back and won't be return in the front for now
    
    * Fetching three movie
    
    Creating an array (I think?) to contain the three movie titles with a keyword, still couldn't manage to show it the HTML tho
    
    * Creation of the question DB
    
    Creation in raw code of all the question that the quiz can ask
    
    * Creation of question and answers
    
    For now, we have two arrays, one with the three false answers and one with the question, the right answer and one keyword to help finding better wrong answers. All of those are currently working separatly
    
    * Keyword in DbFilm
    
    The keyword from DbQuestion is now in DbFilm, but due to my being an idiot, this is the wrong version of DbFilm. Better late than never tho
    
    * Questions almost finished (Back)
    
    The code creates 2 arrays, one with the question, the right answer, and a keyword using for the fake answer. The second array has three fake answers looking like the real one
    
    * Display of question and all answers
    
    We display in basic HTML the quiz, we need to display the answers randomly

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import fetchRandomMovie from "./DbFilm";
+import randomQuestion from "./DbQuestion";
+import fetchedTitles from "./DbFilm";
 function App() {
+  const [randomMovie, setRandomMovie] = useState(0);
+
+  useEffect(() => {
+    // Fetch a random movie when the component mounts
+    fetchRandomMovie(3).then((movie) => {
+      console.log("fetched movie", movie);
+      setRandomMovie(movie);
+    });
+  }, []);
+
+  const WrongMovies = () => {
+    if (typeof randomMovie == "object") {
+      const allMovies = randomMovie.map((movie, index) => {
+        // HTML code created for each movie found
+        return <h2 key={index}>{movie}</h2>;
+      });
+      return <div>{allMovies}</div>;
+    }
+    return <div></div>;
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <h1>Random Movie</h1>
+      <div>
+        <h2>{randomQuestion[0]}</h2>
+        <br />
+        <h2>{randomQuestion[1]}</h2>
+
+        <WrongMovies />
+      </div>
     </div>
   );
 }
